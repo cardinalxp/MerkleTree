9187a7f88b8adb09ebf14267ad876f6bf1595aa4
Forte Company <forte_s@outlook.com>
2023-01-17 18:37:07 -0500

    Add project files.

---
+++ MT.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Dynamic;
+using System.Security.Cryptography;
+
+namespace MerkleTree
+{
+    internal class MT
+    {
+        public static string buidMerkle(string[] transactions)
+        {
+            while (true)
+            {
+                if (transactions.Length == 1) return transactions[0];
+                List<string> list = new List<string>();
+                int length = (transactions.Length % 2 != 0) ? transactions.Length - 1 : transactions.Length;
+                for (int i = 0; i < length; i += 2) list.Add(createHash(transactions[i], transactions[i + 1]));
+                if (length < transactions.Length) list.Add(createHash(transactions[transactions.Length - 1], transactions[transactions.Length - 1]));
+                transactions = list.ToArray();
+            }
+        }
+
+
+        static string createHash(string first, string second)
+        {
+            byte[] array1 = Enumerable.Range(0, first.Length / 2).Select(x => Convert.ToByte(first.Substring(x * 2, 2), 16)).ToArray();
+            Array.Reverse(array1);
+            byte[] array2 = Enumerable.Range(0, second.Length / 2).Select(x => Convert.ToByte(second.Substring(x * 2, 2), 16)).ToArray();
+            Array.Reverse(array2);
+            var concat = array1.Concat(array2).ToArray();
+            SHA256 sha256 = SHA256.Create();
+            byte[] hash1 = sha256.ComputeHash(concat);
+            byte[] hash2 = sha256.ComputeHash(hash1);
+            Array.Reverse(hash2);
+            return BitConverter.ToString(hash2).Replace("-", "").ToLower();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using MerkleTree;
+using Newtonsoft.Json;
+
+var transactions = new string[] {
+   "aaa","bbb","ccc","ddd","eee"  
+};
+var result = MT.buidMerkle(transactions);
+Console.WriteLine("Transactions:\n" + JsonConvert.SerializeObject(transactions));
+Console.WriteLine("\nMerkle Root:\n" + result);
+Console.WriteLine("\nPress enter to close...");
+Console.Read();
